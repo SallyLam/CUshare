20b5ce77bf7bd4d5e67feb106c3276b6cd691bb4
Zhanhao (Jasper) Liu <jasperlau95@gmail.com>
2018-05-13 14:40:26 +0800

    Update models.js

--- common/models.js
+++ common/models.js
@@ -1,22 +1,21 @@
 /*
-	ARTs E-commerce Transactions Platform
+	CUshare Resource Sharing Platform
 
-  	ARTs E-commerce Transactions Platform is an internet-based sales services for small
-	businesses and individual entrepreneurs to run online stores for artwork. It applies
-	the technologies of database, website design and mobile application development.
-	ARTs aims to provide a convenient platform to carry out transactions on the specific area of artworks and to improve users' quality of life.
+	CUshare allow CUers conveniently share their personal items (such as used textbooks) with each other.
+	User can issue sell request, buy request, or even exchange request if needed on CUshare.
+	People with corresponding needs can respond to the request and settle down time/location
+	of the sharing with you directly on the website.
 
+	@category   View
+	@author     Xinyu (Cynric) Fu 		<xyfu6@cse.cuhk.edu.hk>
+	@author     Zhanhao (Jasper) Liu 	<zhliu6@cse.cuhk.edu.hk>
+	@author     Shinmin (Sally) lin 	<smlin6@cse.cuhk.edu.hk>
+	@author     Jiamin (Vito) Chen 		<jmchen6@cse.cuhk.edu.hk>
 
-  	@category   View
-  	@author     Xinyu (Cynric) Fu 		<xyfu6@cse.cuhk.edu.hk>
-  	@author     Zhanhao (Jasper) Liu 	<zhliu6@cse.cuhk.edu.hk>
-  	@author     Shinmin (Sally) lin 	<smlin6@cse.cuhk.edu.hk>
-  	@author     Jiamin (Vito) Chen 		<jmchen6@cse.cuhk.edu.hk>
-  	@author     PengCheng (Ben) Xu 		<pcxu6@cse.cuhk.edu.hk>
- 	  @copyright  2017 ClubAce(group6)
- 	  @license    https://opensource.org/licenses/MIT
-  	@version    1.2.0	14-4-2017
-  	@link       https://bitbucket.org/group06_csci3100_2017/arts
+	@copyright  2018 CSCI4140 Group 1
+	@license    https://opensource.org/licenses/MIT
+	@version    1.2.0	5-5-2018
+	@link       https://github.com/SallyLam/CUshare
 
 */
 
@@ -35,6 +34,7 @@ module.exports = {
         name: String,
         type: String,
         price: Number,
+	description: String,
         imgSrc: String
     },
     cart:{
